afb0ce029708ed606951cf4155a149ee2332e61c
MireyaCR <mireya.mcr@gmail.com>
2022-09-27 19:03:37 +0000

    ejercicio finalizado

--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,5 +1,7 @@
 // Your code goes here:
-
+function renderPerson(param1,param2,param3,param4,param5) {
+  return `${param1} is a ${param4} years old ${param5} born in ${param2} with ${param3} eyes`;
+}
 
 
   ///*** Do not edit below this line ****/
